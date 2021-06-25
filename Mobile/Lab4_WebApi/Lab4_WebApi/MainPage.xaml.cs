4a000cbc17f4ae7d6810e140a83309ee926f371f
dBobrivnyk <hoxtber@gmail.com>
2021-06-25 18:12:17 +0200

    WebApi Publishing configured

---
+++ Mobile/Lab4_WebApi/Lab4_WebApi/MainPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Lab4_WebApi
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            btnPhoto.Clicked += btnPhoto_Clicked;
+        }
+
+        private async void btnPhoto_Clicked(object sender, EventArgs e)
+        {
+            var photo = await Plugin.Media.CrossMedia.Current.TakePhotoAsync(new Plugin.Media.Abstractions.StoreCameraMediaOptions
+            {
+
+            });
+
+            if (photo != null)
+            {
+                imgPhoto.Source = ImageSource.FromStream(() => photo.GetStream());
+                byte[] bytes;
+                using (var memoryStream = new MemoryStream())
+                {
+                    photo.GetStream().CopyTo(memoryStream);
+                    bytes = memoryStream.ToArray();
+                }
+
+                string base64 = Convert.ToBase64String(bytes);
+                //person.PictureBase64 = base64;
+            }
+        }
+    }
+}
---
+++ Mobile/Lab4_WebApi/PeopleStoreApp.Web/Controllers/PeopleController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PeopleStoreApp.Web.Database;
+using PeopleStoreAppDataContracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PeopleStoreApp.Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PeopleController : ControllerBase
+    {
+        private readonly LocalDataStorage db;
+        public PeopleController(LocalDataStorage db)
+        {
+            this.db = db;
+        }
+
+        [HttpGet]
+        public IActionResult GetPeople()
+        {
+            //It's fetching data, and shows in json
+            return Ok(db.People);
+        }
+
+        [HttpPost]
+        public IActionResult AddPerson([FromBody] Person person)//What this frombody means ?
+        {
+            db.AddPerson(person);
+            return Ok();
+        }
+
+        [HttpGet("(id)/photo")]
+        public IActionResult GetPhoto([FromRoute] int id)
+        {
+            var p = db.People.First(w => w.Id == id);
+            return base.File(Convert.FromBase64String(p.PictureBase64), "image/jpeg");
+        }
+    }
+}
