53961575c2df8a9389624286d30d4e9f078b9801
dBobrivnyk <hoxtber@gmail.com>
2021-07-01 17:56:59 +0200

    Project

---
+++ IOT/IoT_Lab1_JWT/Lab1_bobrivnyk.Rest/Context/AzureDbContext.cs
@@ -0,0 +1,35 @@
+﻿using Lab1_bobrivnyk.Rest.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+
+namespace Lab1_bobrivnyk.Rest.Context
+{
+    public class AzureDbContext : DbContext
+    {
+        //this is a bad aproach, but I can't get this data from properties
+        private readonly string configurationString = "Server=tcp:lab3-database-server.database.windows.net,1433;Initial Catalog=bobrivnykLab1-cdv;Persist Security Info=False;" +
+            "User ID=dbobrivnyk;Password=VeryStrongPassword@1;MultipleActiveResultSets=False;Encrypt=True;" +
+            "TrustServerCertificate=False;Connection Timeout=30;";
+        public AzureDbContext()
+        {
+
+        }
+        public AzureDbContext(DbContextOptions<AzureDbContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer(configurationString);
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+
+
+}
---
+++ IOT/IoT_Lab1_JWT/Lab1_bobrivnyk.Rest/Services/UserService.cs
@@ -0,0 +1,84 @@
+﻿using AutoMapper;
+using Lab1_bobrivnyk.Rest.Configurations;
+using Lab1_bobrivnyk.Rest.Context;
+using Lab1_bobrivnyk.Rest.DTOs;
+using Lab1_bobrivnyk.Rest.Models;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1_bobrivnyk.Rest.Services
+{
+    public interface IUserService
+    {
+        AuthenticateResponse Authenticate(AuthenticateRequest model);
+        IEnumerable<User> GetAll();
+        User GetById(int id);
+    }
+
+    public class UserService : IUserService
+    {
+        // users hardcoded for simplicity, store in a db with hashed passwords in production applications
+        private List<User> _users = new List<User>
+                {
+                    new User { Id = 1, Email = "test", PasswordHash = "test" }
+                };
+
+        private readonly AppSettings _appSettings;
+        private AzureDbContext _context;
+        private readonly IMapper _mapper;
+
+        public UserService(IOptions<AppSettings> appSettings/*, AzureDbContext context, IMapper mapper*/)
+        {
+            _appSettings = appSettings.Value;
+            /*_context = context;
+            _mapper = mapper;*/
+        }
+
+        public AuthenticateResponse Authenticate(AuthenticateRequest DTO)
+        {
+            var user = _users.SingleOrDefault(x => x.Email == DTO.Email && x.PasswordHash == DTO.Password);
+
+            // return null if user not found
+            if (user == null) return null;
+
+            // authentication successful so generate jwt token
+            var token = generateJwtToken(user);
+
+            return new AuthenticateResponse(user, token);
+        }
+
+        public IEnumerable<User> GetAll()
+        {
+            return _users;
+        }
+
+        public User GetById(int id)
+        {
+            return _users.FirstOrDefault(x => x.Id == id);
+        }
+
+        // helper methods
+
+        private string generateJwtToken(User user)
+        {
+            // generate token that is valid for 7 hrs
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(_appSettings.Secret);
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new[] { new Claim("id", user.Id.ToString()) }),
+                Expires = DateTime.UtcNow.AddHours(7),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            return tokenHandler.WriteToken(token);
+        }
+    }
+}
